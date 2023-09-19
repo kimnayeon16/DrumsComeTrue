732197b1daeca71b4c320fed534091ecc2dc76d0
이진서 <lalaququ@naver.com>
2023-09-19 10:30:07 +0900

    hotfix: password encoder 해제

--- BE/DrumComesTrue/src/main/java/com/example/drumcomestrue/common/config/PasswordEncorderConfig.java
+++ BE/DrumComesTrue/src/main/java/com/example/drumcomestrue/common/config/PasswordEncorderConfig.java
@@ -1,16 +1,16 @@
-package com.example.drumcomestrue.common.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-@Configuration
-public class PasswordEncorderConfig{
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		//return new MessageDigestPasswordEncoder("SHA-256");
-		return new BCryptPasswordEncoder();
-	}
-}
+// package com.example.drumcomestrue.common.config;
+//
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.security.crypto.password.PasswordEncoder;
+//
+// @Configuration
+// public class PasswordEncorderConfig{
+//
+// 	@Bean
+// 	public PasswordEncoder passwordEncoder() {
+// 		//return new MessageDigestPasswordEncoder("SHA-256");
+// 		return new BCryptPasswordEncoder();
+// 	}
+// }
