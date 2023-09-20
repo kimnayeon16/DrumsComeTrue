fad80230f4123a7e30a7347f5548036db8a61511
김나연 <nayeon0016@gmail.com>
2023-09-20 23:40:43 +0900

    feat: 연주시 시작 카운트 만들기, 드럼 악보 임시 숫자로 띄우기

--- FE/app/src/main/java/com/ssafy/drumscometrue/kpop/KpopPlayActivity.kt
+++ FE/app/src/main/java/com/ssafy/drumscometrue/kpop/KpopPlayActivity.kt
@@ -1,109 +1,115 @@
 package com.ssafy.drumscometrue.kpop
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import android.widget.TextView
 import com.ssafy.drumscometrue.R
+import org.json.JSONArray
 import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStream
 import java.io.InputStreamReader
+import java.util.Timer
+import kotlin.concurrent.timer
 
 class KpopPlayActivity : AppCompatActivity() {
 
     private var mediaPlayer : MediaPlayer ?= null
 
+    private var startTime = 5
+    private var songTime = 0
+    private var timerTask: Timer ?= null
+    private var timerSong: Timer ?= null
+    private lateinit var handler: Handler
+    private lateinit var countTime: TextView
+    private var myRhythm: TextView ?= null
 
-    private fun readJsonFromAssets(fileName: String): String {
-        val json: String
-        try {
-            val inputStream: InputStream = assets.open(fileName)
-            val bufferedReader = BufferedReader(InputStreamReader(inputStream))
-            val stringBuilder = StringBuilder()
-            var line: String?
-            while (bufferedReader.readLine().also { line = it } != null) {
-                stringBuilder.append(line)
-            }
-            json = stringBuilder.toString()
-        } catch (e: IOException) {
-            e.printStackTrace()
-            return ""
-        }
-        return json
-    }
+    private var drumData: JSONArray? = null
 
-    private val handler = Handler()
-    private lateinit var textView: TextView
+    private var iDuration: Long = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kpop_play)
 
-//        mediaPlayer = MediaPlayer.create(this, R.raw.threebears)
-//        mediaPlayer?.start()
+        countTime = findViewById(R.id.countTime)
+        myRhythm = findViewById(R.id.myRhythm)
+        handler = Handler()
 
-        textView = findViewById(R.id.myRhythm)
+        val jsonString = assets.open("ThreeBears.json").reader().readText()
+        Log.d("JSON STR", jsonString)
 
-        val json = readJsonFromAssets("ThreeBears.json")
-        val jsonObject = JSONObject(json)
-        val threeBears = jsonObject.getJSONArray("ThreeBears").getJSONObject(0)
+        drumData  = JSONArray(jsonString)
+        Log.d("json str", drumData.toString())
 
-        // 1초마다 UI 업데이트
-        val intervalMillis = 1000L
-        val startTime = 0.0
-        var currentTime = startTime
+        startCountdown()
 
-        handler.postDelayed(object : Runnable {
-            override fun run() {
-                val notesArray = mutableListOf<String>()
-                val jsonArray = threeBears.optJSONArray(currentTime.toString())
-                if (jsonArray != null) {
-                    for (i in 0 until jsonArray.length()) {
-                        val value = jsonArray.getString(i)
-                        notesArray.add(value)
-                    }
-                }
-
-// notesArray를 문자열 배열로 변환
-                val notes = notesArray.toTypedArray()
-
-
-// 이제 notes를 사용하여 joinToString 함수를 호출할 수 있습니다.
-                textView.text = notes.joinToString(", ")
-                System.out.println(textView.text)
-
-                // 다음 시간으로 이동
-                currentTime += 0.01
-
-                // 종료 시간 설정
-                val endTime = 50.0
-                if (currentTime <= endTime) {
-                    handler.postDelayed(this, intervalMillis)
-                }else{
-                    mediaPlayer = MediaPlayer.create(this@KpopPlayActivity, R.raw.threebears)
-                    mediaPlayer?.start()
-                }
-            }
-        }, intervalMillis)
-
-        handler.postDelayed({
-            textView.text = "3"
-            handler.postDelayed({
-                textView.text = "2"
-                handler.postDelayed({
-                    textView.text = "1"
-                    handler.postDelayed({
-                        textView.text = "Start"
-                    }, 1000) // 1 초 대기
-                }, 1000) // 1 초 대기
-            }, 1000) // 1 초 대기
-        }, 3000) // 3 초 대기
-
+        timerSong = timer(period = 10) {
+            songTime += 10 // songTime을 1/1000초 단위로 증가
+            handleDrumData() // 현재 시간에 해당하는 drum 데이터 처리
+        }
     }
 
+    //곡 연주 시작 전 카운트다운
+    private fun startCountdown() {
+        startTime = startTime.toString().toInt()*100
+        timerTask = timer(period = 10){
+            val sec = startTime / 100
+            runOnUiThread {
+                if (sec > 1) {
+                    countTime.text = "${sec-1}"
+                } else if (sec == 1){
+                    countTime.text = "Start"
+                } else{
+                    countTime.text = ""
+                    timerTask?.cancel()
+                    mediaPlayer = MediaPlayer.create(this@KpopPlayActivity, R.raw.threebears)
+                    mediaPlayer?.start()
+                    handleDrumData()
+                }
+            }
+            startTime--
+        }
+    }
+
+    private fun handleDrumData() {
+        val drumMap = mutableMapOf<Double, List<String>>()
+        for (i in 0 until drumData!!.length()) {
+            val entry = drumData!!.getJSONObject(i)
+            val entryTime = entry.getString("time").toDouble()
+            val drumValuesArray = entry.getJSONArray("drum")
+
+            val drumValuesList = mutableListOf<String>()
+            for (j in 0 until drumValuesArray.length()) {
+                val drumValue = drumValuesArray.optString(j, "")
+                drumValuesList.add(drumValue)
+            }
+            drumMap[entryTime] = drumValuesList
+        }
+
+        System.out.println(drumMap)
+
+        // 현재 시간을 계산 (songTime은 1/1000초 단위로 증가한다고 가정)
+        val currentTime = songTime / 1000.0
+
+        val currentDrumValues = drumMap[currentTime]
+        if (currentDrumValues != null) {
+            for (drumValue in currentDrumValues) {
+                // drum 값을 출력하는 방식을 여기에 구현
+                runOnUiThread {
+                    // 이 부분에서 drum 값을 표시하도록 UI 업데이트
+                    myRhythm?.text = drumValue
+
+                }
+            }
+        }
+    }
 
     override fun onStop(){
         super.onStop()
