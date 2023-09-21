984dcd99a7f35875f33f0a0cbb4989f0cd20c777
SuIn Woo <wbo1026@naver.com>
2023-09-21 10:39:52 +0900

    feat: 드럼UI 수정

--- FE/app/src/main/java/com/ssafy/drumscometrue/user/login/LoginResp.kt
+++
@@ -1,8 +0,0 @@
-package com.ssafy.drumscometrue.user.login
-
-import com.google.gson.annotations.SerializedName
-
-data class LoginResp(
-    @SerializedName("userId") val userId: String?,
-    @SerializedName("userPw") val userPw: String?
-)
