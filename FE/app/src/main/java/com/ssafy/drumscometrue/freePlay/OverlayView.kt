298438373d1397f9e6d2125a96b1bd19895b04e7
LeeSW-k <swl9455@naver.com>
2023-09-24 22:09:56 +0900

    feat: mediaPipe->MLKit Pose 모델 변경

--- FE/app/src/main/java/com/ssafy/drumscometrue/freePlay/OverlayView.kt
+++ FE/app/src/main/java/com/ssafy/drumscometrue/freePlay/OverlayView.kt
@@ -22,9 +22,8 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 import androidx.core.content.ContextCompat
-import com.google.mediapipe.tasks.vision.core.RunningMode
-import com.google.mediapipe.tasks.vision.poselandmarker.PoseLandmarker
-import com.google.mediapipe.tasks.vision.poselandmarker.PoseLandmarkerResult
+import com.google.mlkit.vision.pose.Pose
+import com.google.mlkit.vision.pose.PoseLandmark
 import com.ssafy.drumscometrue.R
 import kotlin.math.max
 import kotlin.math.min
@@ -37,11 +36,9 @@ import kotlin.math.min
 class OverlayView(context: Context?, attrs: AttributeSet?) :
     View(context, attrs) {
 
-    private var results: PoseLandmarkerResult? = null//Pose Landmark의 결과 저장하는 변수
-    //Pose 랜드마크 그릴 때 사용되는 Paint객체
+    private var results: Pose? = null
     private var pointPaint = Paint()
     private var linePaint = Paint()
-
     private var scaleFactor: Float = 1f
     private var imageWidth: Int = 1
     private var imageHeight: Int = 1
@@ -50,17 +47,17 @@ class OverlayView(context: Context?, attrs: AttributeSet?) :
         initPaints()
     }
 
-    fun clear() {
-        results = null
-        pointPaint.reset()
-        linePaint.reset()
-        invalidate()
-        initPaints()
+    fun setResults(pose: Pose, imageHeight: Int, imageWidth: Int) {
+        results = pose
+        this.imageHeight = imageHeight
+        this.imageWidth = imageWidth
+//        scaleFactor = max(width * 1f / imageWidth, height * 1f / imageHeight)
+        scaleFactor = max(width * 1f / imageHeight, height * 1f / imageWidth)
+        invalidate() // 뷰를 다시 그리도록 요청
     }
 
     private fun initPaints() {
-        linePaint.color =
-            ContextCompat.getColor(context!!, R.color.mp_color_primary)
+        linePaint.color = ContextCompat.getColor(context!!, R.color.mp_color_secondary)
         linePaint.strokeWidth = LANDMARK_STROKE_WIDTH
         linePaint.style = Paint.Style.STROKE
 
@@ -69,58 +66,120 @@ class OverlayView(context: Context?, attrs: AttributeSet?) :
         pointPaint.style = Paint.Style.FILL
     }
 
-    /**
-     * 뷰가 화면에 그려질 때 호출되는 메서드
-     * 결과를 지우고 Paint객체를 재설정하여 화면 클리어
-     * */
     override fun draw(canvas: Canvas) {
         super.draw(canvas)
-        results?.let { poseLandmarkerResult ->
-            for(landmark in poseLandmarkerResult.landmarks()) {
-                for(normalizedLandmark in landmark) {
-                    canvas.drawPoint(
-                        normalizedLandmark.x() * imageWidth * scaleFactor,
-                        normalizedLandmark.y() * imageHeight * scaleFactor,
-                        pointPaint
-                    )
-                }
+        results?.let { pose ->
+            canvas.save() // 현재 캔버스 상태 저장
 
-                PoseLandmarker.POSE_LANDMARKS.forEach {
-                    canvas.drawLine(
-                        poseLandmarkerResult.landmarks().get(0).get(it!!.start()).x() * imageWidth * scaleFactor,
-                        poseLandmarkerResult.landmarks().get(0).get(it.start()).y() * imageHeight * scaleFactor,
-                        poseLandmarkerResult.landmarks().get(0).get(it.end()).x() * imageWidth * scaleFactor,
-                        poseLandmarkerResult.landmarks().get(0).get(it.end()).y() * imageHeight * scaleFactor,
-                        linePaint)
-                }
+            // 뷰를 가로 방향으로 뒤집기
+            canvas.scale(-1f, 1f, width / 2f, height / 2f)
+
+            for (landmark in pose.allPoseLandmarks) {
+                canvas.drawPoint(
+                    landmark.position.x * scaleFactor, // 좌표를 scaleFactor로 조정
+                    landmark.position.y * scaleFactor, // 좌표를 scaleFactor로 조정
+                    pointPaint
+                )
+                val nose = pose.getPoseLandmark(PoseLandmark.NOSE)
+                val lefyEyeInner = pose.getPoseLandmark(PoseLandmark.LEFT_EYE_INNER)
+                val lefyEye = pose.getPoseLandmark(PoseLandmark.LEFT_EYE)
+                val leftEyeOuter = pose.getPoseLandmark(PoseLandmark.LEFT_EYE_OUTER)
+                val rightEyeInner = pose.getPoseLandmark(PoseLandmark.RIGHT_EYE_INNER)
+                val rightEye = pose.getPoseLandmark(PoseLandmark.RIGHT_EYE)
+                val rightEyeOuter = pose.getPoseLandmark(PoseLandmark.RIGHT_EYE_OUTER)
+                val leftEar = pose.getPoseLandmark(PoseLandmark.LEFT_EAR)
+                val rightEar = pose.getPoseLandmark(PoseLandmark.RIGHT_EAR)
+                val leftMouth = pose.getPoseLandmark(PoseLandmark.LEFT_MOUTH)
+                val rightMouth = pose.getPoseLandmark(PoseLandmark.RIGHT_MOUTH)
+
+                val leftShoulder = pose.getPoseLandmark(PoseLandmark.LEFT_SHOULDER)
+                val rightShoulder = pose.getPoseLandmark(PoseLandmark.RIGHT_SHOULDER)
+                val leftElbow = pose.getPoseLandmark(PoseLandmark.LEFT_ELBOW)
+                val rightElbow = pose.getPoseLandmark(PoseLandmark.RIGHT_ELBOW)
+                val leftWrist = pose.getPoseLandmark(PoseLandmark.LEFT_WRIST)
+                val rightWrist = pose.getPoseLandmark(PoseLandmark.RIGHT_WRIST)
+                val leftHip = pose.getPoseLandmark(PoseLandmark.LEFT_HIP)
+                val rightHip = pose.getPoseLandmark(PoseLandmark.RIGHT_HIP)
+                val leftKnee = pose.getPoseLandmark(PoseLandmark.LEFT_KNEE)
+                val rightKnee = pose.getPoseLandmark(PoseLandmark.RIGHT_KNEE)
+                val leftAnkle = pose.getPoseLandmark(PoseLandmark.LEFT_ANKLE)
+                val rightAnkle = pose.getPoseLandmark(PoseLandmark.RIGHT_ANKLE)
+
+                val leftPinky = pose.getPoseLandmark(PoseLandmark.LEFT_PINKY)
+                val rightPinky = pose.getPoseLandmark(PoseLandmark.RIGHT_PINKY)
+                val leftIndex = pose.getPoseLandmark(PoseLandmark.LEFT_INDEX)
+                val rightIndex = pose.getPoseLandmark(PoseLandmark.RIGHT_INDEX)
+                val leftThumb = pose.getPoseLandmark(PoseLandmark.LEFT_THUMB)
+                val rightThumb = pose.getPoseLandmark(PoseLandmark.RIGHT_THUMB)
+                val leftHeel = pose.getPoseLandmark(PoseLandmark.LEFT_HEEL)
+                val rightHeel = pose.getPoseLandmark(PoseLandmark.RIGHT_HEEL)
+                val leftFootIndex = pose.getPoseLandmark(PoseLandmark.LEFT_FOOT_INDEX)
+                val rightFootIndex = pose.getPoseLandmark(PoseLandmark.RIGHT_FOOT_INDEX)
+
+                // Face
+                drawLine(canvas, nose, lefyEyeInner, linePaint)
+                drawLine(canvas, lefyEyeInner, lefyEye, linePaint)
+                drawLine(canvas, lefyEye, leftEyeOuter, linePaint)
+                drawLine(canvas, leftEyeOuter, leftEar, linePaint)
+                drawLine(canvas, nose, rightEyeInner, linePaint)
+                drawLine(canvas, rightEyeInner, rightEye, linePaint)
+                drawLine(canvas, rightEye, rightEyeOuter, linePaint)
+                drawLine(canvas, rightEyeOuter, rightEar, linePaint)
+                drawLine(canvas, leftMouth, rightMouth, linePaint)
+
+                drawLine(canvas, leftShoulder, rightShoulder, linePaint)
+                drawLine(canvas, leftHip, rightHip, linePaint)
+
+                // Left body
+                drawLine(canvas, leftShoulder, leftElbow, linePaint)
+                drawLine(canvas, leftElbow, leftWrist, linePaint)
+                drawLine(canvas, leftShoulder, leftHip, linePaint)
+                drawLine(canvas, leftHip, leftKnee, linePaint)
+                drawLine(canvas, leftKnee, leftAnkle, linePaint)
+                drawLine(canvas, leftWrist, leftThumb, linePaint)
+                drawLine(canvas, leftWrist, leftPinky, linePaint)
+                drawLine(canvas, leftWrist, leftIndex, linePaint)
+                drawLine(canvas, leftIndex, leftPinky, linePaint)
+                drawLine(canvas, leftAnkle, leftHeel, linePaint)
+                drawLine(canvas, leftHeel, leftFootIndex, linePaint)
+
+                // Right body
+                drawLine(canvas, rightShoulder, rightElbow, linePaint)
+                drawLine(canvas, rightElbow, rightWrist, linePaint)
+                drawLine(canvas, rightShoulder, rightHip, linePaint)
+                drawLine(canvas, rightHip, rightKnee, linePaint)
+                drawLine(canvas, rightKnee, rightAnkle, linePaint)
+                drawLine(canvas, rightWrist, rightThumb, linePaint)
+                drawLine(canvas, rightWrist, rightPinky, linePaint)
+                drawLine(canvas, rightWrist, rightIndex, linePaint)
+                drawLine(canvas, rightIndex, rightPinky, linePaint)
+                drawLine(canvas, rightAnkle, rightHeel, linePaint)
+                drawLine(canvas, rightHeel, rightFootIndex, linePaint)
             }
         }
     }
 
-    fun setResults(
-        poseLandmarkerResults: PoseLandmarkerResult,
-        imageHeight: Int,
-        imageWidth: Int,
-        runningMode: RunningMode = RunningMode.IMAGE
-    ) {
-        results = poseLandmarkerResults
+    fun drawLine(
+        canvas: Canvas,
+        startLandmark: PoseLandmark?,
+        endLandmark: PoseLandmark?,
+        paint: Paint
+    ){
+        val start = startLandmark!!.position3D
+        val end = endLandmark!!.position3D
 
-        this.imageHeight = imageHeight
-        this.imageWidth = imageWidth
+        // Gets average z for the current body line
+        val avgZInImagePixel = (start.z + end.z) / 2
 
-        scaleFactor = when (runningMode) {
-            RunningMode.IMAGE,
-            RunningMode.VIDEO -> {
-                min(width * 1f / imageWidth, height * 1f / imageHeight)
-            }
-            RunningMode.LIVE_STREAM -> {
-                // PreviewView is in FILL_START mode. So we need to scale up the
-                // landmarks to match with the size that the captured images will be
-                // displayed.
-                max(width * 1f / imageWidth, height * 1f / imageHeight)
-            }
-        }
-        invalidate()
+        canvas.drawLine(
+            start.x * scaleFactor,
+            start.y * scaleFactor,
+            end.x * scaleFactor,
+            end.y * scaleFactor,
+            paint
+        )
+
+
     }
 
     companion object {
