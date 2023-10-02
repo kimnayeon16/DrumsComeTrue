82a834e51a9b2ec52aa99cd802506a88fbcf0880
DESKTOP-BLHP263\SSAFY <qeqe0456@naver.com>
2023-10-02 19:34:06 +0900

    feat/tutorial 튜토리얼 페이지 구성

--- FE/app/src/main/java/com/ssafy/drumscometrue/tutorial/TutorialActivity.kt
+++ FE/app/src/main/java/com/ssafy/drumscometrue/tutorial/TutorialActivity.kt
@@ -15,15 +15,15 @@ import com.ssafy.drumscometrue.freePlay.fragment.CameraFragment
 
 class TutorialActivity : AppCompatActivity() {
     private val videoItems = listOf(
-        VideoItem("JV7OcwKU734",R.drawable.tutor_img_snaredrum, "튜토리얼 1", "스네어 연주법","1. 스틱 팁(머리)를 중앙에 둔다\n\n" +
+        VideoItem("6HPaIVG2Qpc",R.drawable.tutor_img_snaredrum, "튜토리얼 1", "스네어 연주법","1. 스틱 팁(머리)을 중앙에 둔다.\n\n" +
                 "2. 이때 스틱은 스네어와 2-3cm 높이로 수평\n\n" +
-                "3. 손목을 들어줬다가 내려친다"),
-        VideoItem("30ooSe0yoVs", R.drawable.tutor_img_hihatdrum,"튜토리얼 2", "하이햇 연주법", "1.손목에 힘을 풀고 스틱을 가볍게 쥔다 \n\n" +
-                "2. 가볍게 손목힘으로 툭툭 친다 \n\n" +
-                "3. 하이햇과 스네어를 같이 연주 할 경우 하이햇도 손목을 들어올린다."),
-        VideoItem("9iRzQzWkdGY", R.drawable.tutor_img_basedrum,"튜토리얼 2", "베이스드럼 연주법", "1. 연주중이 아닐 땐 비터 베이스에 붙이고 있기 \n\n" +
-                "2-1 Heel Up 연주법. \n 발 뒤꿈치를 들고 페달에 발의 앞부분을 붙인 상태로 다리를 살짝 들어서 밟기.\n\n" +
-                "2-2 Heel Down 연주법. \n 발 뒤꿈치를 페달에 붙이고 앞발을 들어 페달을 밟기."),
+                "3. 손목을 들어줬다가 내려친다."),
+        VideoItem("D9gWRLOe6zk", R.drawable.tutor_img_hihatdrum,"튜토리얼 2", "하이햇 연주법", "1.손목에 힘을 풀고 스틱을 가볍게 쥔다. \n\n" +
+                "2. 스틱의 어깨부분으로 하이햇의 테두리를 손목을 꺾어 툭툭 친다. \n\n" +
+                "3. 하이햇과 스네어를 같이 연주 할 경우 하이햇을 연주할 때에 손목을 들어올린다."),
+        VideoItem("eu7ULodUy8U", R.drawable.tutor_img_basedrum,"튜토리얼 3", "베이스드럼 연주법", "1. 연주중이 아닐 땐 비터 베이스에 붙이고 있는다 \n\n" +
+                "2-1 Heel Up 연주법. \n 발 뒤꿈치를 들고 페달에 발의 앞부분을 붙인 상태로 다리를 살짝 들어서 밟는다.\n\n" +
+                "2-2 Heel Down 연주법. \n 발 뒤꿈치를 페달에 붙이고 앞발을 들어 페달을 밟는다."),
     )
 
     private var currentPage: Int = 0
